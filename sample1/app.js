6b2f512f1681167e847e8f11dda93d140adf1f8a
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2021-01-10 11:05:58 +0100

    Update app.js

--- sample1/app.js
+++ sample1/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('sample1 nodejs app, branch[branch2] version [1] !\n');
+  res.send('sample1 nodejs app, branch[branch3] version [2] !\n');
 });
 
 app.listen(8080, function () {
