47be1014259857d6e6646f0b6e24b26507a01330
Marco Antonioni <marcoantonioni@users.noreply.github.com>
2021-01-10 10:26:59 +0100

    Update app.js

--- sample1/app.js
+++ sample1/app.js
@@ -1,1 +1,10 @@
-...
+var express = require('express');
+app = express();
+
+app.get('/', function (req, res) {
+  res.send('sample1 nodejs app, branch[main] version [1] !\n');
+});
+
+app.listen(8080, function () {
+  console.log('sample app listening on port 8080!');
+});
